4ea799a7fabfc2ec0bb1bfdb3d5d36b604fd207b
Henry Wang <henry.t.wang@gmail.com>
2013-07-28 18:05:20 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeadCollector::Application.config.secret_token = '96c2a1b9da03de1b56803a56b17ddf211f8782384d01288f22e8e0d0fc62120bbe86df760ae632360ec0b8651c98712326db8583ebf8d0536212261748a4f0f3'
